4ba62c48c5e45abc3927908e0503a20d3190e6bf
junseinagao <junsei.nagao@gmail.com>
2024-06-24 22:08:57 +0900

    💄 `_index.jsx` を更新して `new.css` でスタイリングしました

--- app/root.jsx
+++ app/root.jsx
@@ -5,6 +5,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { NavigationMenu } from "./components/NavigationMenu";
+
+export const meta = () => {
+  return [
+    { title: "Geek Twitter (Remix and Firebase)" },
+    {
+      name: "description",
+      content: "RemixとFirebaseを利用したTodoアプリケーションのサンプルです",
+    },
+  ];
+};
 
 export function Layout({ children }) {
   return (
@@ -12,6 +23,10 @@ export function Layout({ children }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1/new.min.css"
+        ></link>
         <Meta />
         <Links />
       </head>
