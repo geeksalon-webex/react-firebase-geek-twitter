2542700c03c7e70b3f28b71ed202b2df5dba8c9f
yamamoto <k221754@kansai-u.ac.jp>
2024-06-24 23:40:36 +0900

    linkを追加しました

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,7 +1,12 @@
+import { Link } from "@remix-run/react";
+
 export default function Index() {
   return (
     <div>
       <h1>トップページ</h1>
+      <p>
+        <Link to="/posts/new">新規投稿ページへ</Link>
+      </p>
     </div>
   );
 }
