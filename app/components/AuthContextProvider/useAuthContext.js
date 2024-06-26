f694d978af35a0f21847f4274d56a07522a163c6
junseinagao <junsei.nagao@gmail.com>
2024-06-26 12:51:05 +0900

    `authContextInitialState` に jsDocコメントを付けました

--- app/components/AuthContextProvider/useAuthContext.js
+++ app/components/AuthContextProvider/useAuthContext.js
@@ -1,5 +1,17 @@
 import { createContext, useContext } from "react";
 
+/**
+ * このコメントは jsdoc といい、TypeScriptの型定義をコメントとして書くための記法。
+ * `user` に型定義を付けて、`user`の持つプロパティを他の箇所でVS Code越しに知りたいので使用している
+ * @typedef {Object} AuthContextInitialState
+ * @property {boolean} isInitialized
+ * @property {boolean} isSignedIn
+ * @property {import("firebase/auth").User | null} user
+ */
+
+/**
+ * @type {AuthContextInitialState}
+ */
 export const authContextInitialState = {
   isInitialized: false,
   isSignedIn: false,
