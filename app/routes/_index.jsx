d494b6e3e4c5637a8607e2d83baf1b85391a640a
junseinagao <junsei.nagao@gmail.com>
2024-06-27 01:34:25 +0900

    ✨ firestoreを使った一覧表示のサンプルを追加しました

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import { Link } from "@remix-run/react";
 import { NewPostForm } from "../components/NewPostForm/NewPostForm";
+import { PostList } from "../components/PostList/PostList";
 
 export default function Index() {
+  const [triggerKey, setTriggerKey] = useState(0);
   return (
     <div>
       <h1>トップページ</h1>
-      <NewPostForm />
+      <NewPostForm
+        onSuccess={() => {
+          setTriggerKey((prev) => prev + 1);
+        }}
+      />
+      <PostList triggerKey={triggerKey} />
       <p>
         <Link to="/posts/new">新規投稿ページへ</Link>
       </p>
