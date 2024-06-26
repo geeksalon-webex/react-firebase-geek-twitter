429d0ec66a8e0d1ef8c72e85d17e2d6bc4e54263
junseinagao <junsei.nagao@gmail.com>
2024-06-26 12:52:57 +0900

    ✨ firestoreを使った投稿機能のサンプルを追加しました

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,9 +1,11 @@
 import { Link } from "@remix-run/react";
+import { NewPostForm } from "../components/NewPostForm/NewPostForm";
 
 export default function Index() {
   return (
     <div>
       <h1>トップページ</h1>
+      <NewPostForm />
       <p>
         <Link to="/posts/new">新規投稿ページへ</Link>
       </p>
--- app/routes/posts.new.jsx
+++ app/routes/posts.new.jsx
@@ -1,13 +1,21 @@
-import { Link } from "@remix-run/react";
+import { Link, useNavigate } from "@remix-run/react";
 import { useGuardAuthPage } from "../components/AuthContextProvider/useGuardAuthPage";
+import { NewPostForm } from "../components/NewPostForm/NewPostForm";
 
 export default function NewPostPage() {
   const isEnableShowPage = useGuardAuthPage();
+  const navigate = useNavigate();
 
   if (!isEnableShowPage) return null;
   return (
     <div>
       <h1>新規投稿ページ</h1>
+      <NewPostForm
+        onSuccess={() => {
+          // 投稿後にトップページに移動する
+          navigate("/");
+        }}
+      />
       <p>
         <Link to="/">トップページへ</Link>
       </p>
